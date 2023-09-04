868b121c38a85bfa311f9b79e205734b30ba0479
Husnain <husnainalirafique125@gmail.com>
2023-09-04 22:45:52 +0500

    SharedPref

--- app/src/main/java/com/example/sharedpref/MainActivity.kt
+++ app/src/main/java/com/example/sharedpref/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.sharedpref
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display.Mode
+import androidx.databinding.DataBindingUtil
+import com.example.sharedpref.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.btnClick.setOnClickListener {
+            settingSharedPref()
+        }
+
+        Snackbar.make(window.decorView, "${gettingSharedPref()}", Snackbar.LENGTH_SHORT).show()
     }
 
+    private fun settingSharedPref() {
+        sharedPref = getSharedPreferences("key", MODE_PRIVATE)
+        sharedPref.edit().apply {
+            putBoolean("login", true)
+        }.apply()
+    }
+
+    private fun gettingSharedPref(): Boolean {
+        return sharedPref.getBoolean("login", false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
+    }
 }
